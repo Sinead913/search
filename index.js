1761a58f64a43ccd891ba6bdd400dd41127506fd
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 22:50:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 
+
 const db = mysql.createConnection ({
   user: 'root',
   password: ${{ secrets.GCLOUD_PASSWORD }},
