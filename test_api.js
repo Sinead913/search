f26521b4960361b8766cbbca5ae5b14f843ccfb1
Sinead913 <57669285+Sinead913@users.noreply.github.com>
2020-01-12 22:04:30 +0000

    Update test_api.js

--- test_api.js
+++ test_api.js
@@ -7,25 +7,12 @@ describe("API test",function(){
   it("should return a 200 response",function(done){
 
     server.get("/")
-    .query({ search: test })
+    .query({ search: 'test' })
     .expect("Content-type",/json/)
     .expect(200)
     .end(function(err,res){
       res.status.should.equal(200);
       res.body.error.should.equal(false);
-      done();
-    });
-  });
-
-  it("should return correct response body",function(done){
-
-    server.get("/")
-    .query({ search: 'network' })
-    .expect("Content-type",/json/)
-    .expect(200)
-    .end(function(err,res){
-      res.body.error.should.equal(false);
-      res.body.answer.should.equal(["https://en.wikipedia.org/wiki/FreeNATS","https://en.wikipedia.org/wiki/Network_monitoring","https://en.wikipedia.org/wiki/Service-level_agreement","https://en.wikipedia.org/wiki/High_availability","https://en.wikipedia.org/wiki/Network_traffic_measurement"]);
       done();
     });
   });
